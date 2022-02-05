3fc9de94a6d0caafc6ff4aee285e9a6582a9313a
DT333TJ <1099707725@qq.com>
2022-02-05 19:10:57 +0800

    添加vite-vue微应用

---
+++ microapps-vite-vue/src/views/home.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  // defineProps < { msg: string } > ()
+
+  const count = ref(0)
+</script>
+
+<template>
+  <div class="p-home">This is a page for Home</div>
+  <router-link to="/demo" class="p-home">Go to Demo</router-link>
+</template>
+
+<style lang="scss" scoped>
+.p-home {
+  color: pink
+}
+</style>
